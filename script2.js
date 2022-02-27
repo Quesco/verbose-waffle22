eb92d1ec13096adee7910eb9ffda6beec17b3e08
Quesco <99868373+Quesco@users.noreply.github.com>
2022-02-28 00:59:56 +0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+function multi(){
+    let inp = document.getElementById('inp_1').Value,
+    inp_2 = document.getElementById('inp_2').Value;
+
+    console.log(inp1*inp2);
+    function toggleText(inp1*inp2)    
+ 
+}
